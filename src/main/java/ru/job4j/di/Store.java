7f83eb160a704f9b7d6d1200c451bb2ed79f37a2
WhiteVax <WhiteVax@gmail.com>
2023-01-12 20:19:24 +0200

    4. Время жизни (Scope).

--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -1,11 +1,23 @@
 package ru.job4j.di;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * В Spring можно внедрять режимы для объекта, всего их 6. По умолчанию 1.
+ * 1. singleton - объект создает один раз на всю виртуальную машину.
+ * 2. prototype - каждый раз создается новый объект.
+ * 3. session - объект существует, пока существует сессия пользователя.
+ * 4. request - объект существует, пока существует запрос.
+ * 5. application - объект существует, пока существует ServletContext.
+ * 6. websocket - объект существует, пока есть активная сессия для web-сокета.
+ */
+
 @Component
+@Scope("prototype")
 public class Store {
     private List<String> data = new ArrayList<String>();
 
