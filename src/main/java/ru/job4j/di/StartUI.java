3f39f1c4eeed5985a83df2e8f655bc5cc976b91d
WhiteVax <WhiteVax@gmail.com>
2023-01-12 20:12:19 +0200

    3. @Autowired. Внедрение зависимости.

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,17 +1,22 @@
 package ru.job4j.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+/**
+ * С помощью аннотации Autowired можно внедрять зависимости через:
+ * - через конструктор. Правильный способ.
+ * - через поле и аннотацию @Autowired. Нежелательный способ.
+ * - через метод set и аннотацию @Autowired. Нежелательный способ.
+ */
 
 @Component
 public class StartUI {
 
+    @Autowired
     private Store store;
+    @Autowired
     private ConsoleInput input;
-
-    public StartUI(Store store, ConsoleInput input) {
-        this.input = input;
-        this.store = store;
-    }
 
     public void add(String value) {
         store.add(value);
